fe35a4173faa51372eb9a43957d73559df43a2de
hseetas25 <balagasateeshspis@gmail.com>
2021-04-03 00:26:33 +0530

    Added new components

--- angularfos/src/app/app-routing.module.ts
+++ angularfos/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ProductListComponent},
+  {path:"",component:ProductAlertsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
